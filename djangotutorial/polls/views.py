df02b152d9619f7539d2bb23decb66e2b9fb5809
Phionanamugga <phionanamugga15@gmail.com>
2025-01-06 19:04:03 +0100

    modifies search for the questions

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
 from django.template import loader
+from django.http import Http404
 # Create your views here.
 
 def index(request):
@@ -12,7 +13,11 @@ def index(request):
 # Leave the rest of the views (detail, results, vote) unchanged
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
@@ -21,6 +26,9 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+
+
 
 
 
