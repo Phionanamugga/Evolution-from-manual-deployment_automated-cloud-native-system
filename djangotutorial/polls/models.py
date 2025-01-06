499e21b046384606e5aac13009b262ec338c2628
Phionanamugga <phionanamugga15@gmail.com>
2025-01-06 22:45:25 +0100

    modifies index view

--- djangotutorial/polls/models.py
+++ djangotutorial/polls/models.py
@@ -8,10 +8,24 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    def test_was_published_recently_with_old_question(self):
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
 
-
+    """
+    was_published_recently() returns False for questions whose pub_date
+    is older than 1 day.
+    """
+    def test_was_published_recently_with_recent_question(self):
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
+    """
+    was_published_recently() returns True for questions whose pub_date
+    is within the last day.
+    """
+    
 class Choice(models.Model):
     # ...
     def __str__(self):
